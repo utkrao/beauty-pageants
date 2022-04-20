d798d6fe7d2cc7be212716ec4af0f73ce7d49017
utkrao <66346802+utkrao@users.noreply.github.com>
2022-04-21 00:51:02 +0530

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+
+        const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+        if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+            currentlyActiveAccordionItemHeader.classList.toggle("active");
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        }
+
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+
+    });
+});
+
+function uncheck() {
+    document.getElementById("click").checked = false;
+}
